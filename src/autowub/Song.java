99ee4d9ae2a957f1c58437ca8f3bbc85da0c0cef
Connor McLaughlin <connormcl14@gmail.com>
2013-11-09 12:41:26 -0500

    fixed chord algorithm

--- src/autowub/Song.java
+++ src/autowub/Song.java
@@ -11,15 +11,18 @@ public class Song {
 	Verse verse = new Verse(16,4);
 	Bridge bridge = new Bridge();
 	Ending ending = new Ending();
+	static String[][] natChords = new String[][]{{"c","e","g"},{"d","f","a"},{"e","g","b"},{"f","a","c"},{"g","b","d"},{"a","c","e"},{"b","d","f"}};
 	static String[] keys = new String[]{"c","c#","d","d#","e","f","f#","g","g#","a","a#","b"};
 	static String[] natKeys = new String[]{"c","d","e","f","g","a","b"};
 	static String[] sharpNum = new String[]{"c","g","d","a","e","b","f#","c#"};
 	static String[] flatNum = new String[]{"c","f","a#","d#","g#"};
 	static String[]	sharpOrder = new String[]{"f#","c#","g#","d#","a#","e#","b#"};
-	static String[]	flatOrder = new String[]{"a#","d#","g#","d#"};
+	//static String[]	flatOrder = new String[]{"a#","d#","g#","c#"};
+	static String[]	flatOrder = new String[]{"b","e","a","d"};
+	static String[] chordTones = new String[7];
 	String key;
 	int keyIndex;
-	String[][] chords = new String[7][3];
+	static String[][] chords = new String[7][3];
 //	String I[] = new String[3];
 //	String ii[] = new String[3];
 //	String iii[] = new String[3];
@@ -72,80 +75,173 @@ public class Song {
 	public Song(){
 		pickKey();
 		System.out.println("Key: " + key);
+		boolean sharps = false;
+		//assigns index in natural array
 		int natIndex = 0;
-		//System.out.println(keys[keyIndex].substring(0, 1));
 		for(int i = 0; i<natKeys.length; i++){
 			if(natKeys[i].equals(keys[keyIndex].substring(0, 1))){
 				natIndex = i;
 				break;
 			}
 		}
-		for(int j=0; j<chords.length; j++){
-			if(natIndex+j<natKeys.length){
-				chords[j][0] = natKeys[natIndex+j];
-			}else{
-				chords[j][0] = natKeys[natIndex+j-natKeys.length];
-			}
-			if(natIndex+2+j<natKeys.length){
-				chords[j][1] = natKeys[natIndex+2+j];
-			}else if(natIndex+2+j<natKeys.length*2){
-				chords[j][1] = natKeys[natIndex+2+j-natKeys.length];
-			}else{
-				chords[j][1] = natKeys[natIndex+j-natKeys.length];
-			}
-			if(natIndex+4+j<natKeys.length){
-				chords[j][2] = natKeys[natIndex+4+j];
-			}else if(natIndex+4+j<natKeys.length*2){
-				chords[j][2] = natKeys[natIndex+4+j-natKeys.length];
-			}else{
-				chords[j][2] = natKeys[natIndex+4+j-(natKeys.length*2)];
-			}
-		}
-		int accCount = -1;
-		for(int a = 0; a<sharpNum.length; a++){
-			if(key.equals(sharpNum[a])){
-				accCount = a;
-				for(int b = 0; b<accCount; b++){
-					for(int c = 0; c<chords.length; c++){
-						for(int d = 0; d<chords[c].length; d++){
-							if(chords[c][d].equals(sharpOrder[b].substring(0, 1))){
-								chords[c][d] = sharpOrder[b];
-							}
-						}
-					}
-				}
+		int sharpCount = 0;
+		//check if sharps or flats
+		for(int i = 0; i<sharpNum.length; i++){
+			if(key.equals(sharpNum[i])){
+				sharps = true;
+				sharpCount = i;
 				break;
 			}
 		}
-		if(accCount<0){
-			for(int e=0; e<flatNum.length; e++){
-				if(key.equals(flatNum[e])){
-					accCount = e;
-					for(int f = 0; f<accCount; f++){
-						for(int g = 0; g<chords.length; g++){
-							for(int h = 0; h<chords[g].length; h++){
-								if(chords[g][h].equals(flatOrder[f].substring(0, 1))){
-									chords[g][h] = flatOrder[f];
-								}
-							}
+		//assigns sharps where necessary
+		if(sharps){
+			for(int j=0; j<chords.length; j++){
+				if(natIndex+j<natChords.length){
+					chords[j] = natChords[natIndex+j];
+				}else{
+					chords[j] = natChords[natIndex+j-natChords.length];
+				}
+				for(int k=0; k<chords[j].length; k++){
+					for(int a=0; a<sharpCount; a++){
+						//System.out.println(chords[j][k] + "  " + sharpNum[a]);
+						if(chords[j][k].equals(sharpOrder[a].substring(0, 1))){
+							chords[j][k] = sharpOrder[a];
 						}
 					}
+					//System.out.println(chords[j][k]);
+				}
+//				for(int k=0; k<chords[j].length; k++){
+//					System.out.print(chords[j][k]);
+//				}
+//				System.out.println("\n");
+			}
+		}else{
+			int flatCount = 0;
+			for(int i=0; i<flatNum.length; i++){
+				if(key.equals(flatNum[i])&&key.contains("#")){
+					flatCount = i;
+					break;
+				}else if(key.equals(flatNum[i])&&(!key.contains("#"))){
+					flatCount = i;
+					natIndex--;
 					break;
 				}
 			}
+			for(int j=0; j<chords.length; j++){
+				if(natIndex+1+j<natChords.length){
+					chords[j] = natChords[natIndex+1+j];
+				}else{
+					chords[j] = natChords[natIndex+1+j-natChords.length];
+				}
+				for(int k=0; k<chords[j].length; k++){
+					for(int a=0; a<flatCount; a++){
+						if(chords[j][k].equals(flatOrder[a])){
+							int index = 0;
+							for(int b=0; b<keys.length; b++){
+								if(flatOrder[a].equals(keys[b])){
+									index = b;
+									break;
+								}
+							}
+							chords[j][k] = keys[index-1];
+						}
+					}
+				}
+//				for(int k=0; k<chords[j].length; k++){
+//					System.out.print(chords[j][k]);
+//				}
+//				System.out.println("\n");
+			}
 		}
-		for(int j=0; j<chords.length; j++){
-			for(int k=0; k<chords[j].length;k++){
-				System.out.print(chords[j][k]);
+		for(int i=0; i<chords.length; i++){
+			for(int j=0; j<chords[i].length; j++){
+				System.out.print(chords[i][j]);
 			}
 			System.out.print("\n");
 		}
-
 	}
+	
+//	public Song(){
+//		pickKey();
+//		System.out.println("Key: " + key);
+//		int natIndex = 0;
+//		//System.out.println(keys[keyIndex].substring(0, 1));
+//		for(int i = 0; i<natKeys.length; i++){
+//			if(natKeys[i].equals(keys[keyIndex].substring(0, 1))){
+//				natIndex = i;
+//				break;
+//			}
+//		}
+//		for(int j=0; j<chords.length; j++){
+//			if(natIndex+j<natKeys.length){
+//				chords[j][0] = natKeys[natIndex+j];
+//			}else{
+//				chords[j][0] = natKeys[natIndex+j-natKeys.length];
+//			}
+//			if(natIndex+2+j<natKeys.length){
+//				chords[j][1] = natKeys[natIndex+2+j];
+//			}else if(natIndex+2+j<natKeys.length*2){
+//				chords[j][1] = natKeys[natIndex+2+j-natKeys.length];
+//			}else{
+//				chords[j][1] = natKeys[natIndex+j-natKeys.length];
+//			}
+//			if(natIndex+4+j<natKeys.length){
+//				chords[j][2] = natKeys[natIndex+4+j];
+//			}else if(natIndex+4+j<natKeys.length*2){
+//				chords[j][2] = natKeys[natIndex+4+j-natKeys.length];
+//			}else{
+//				chords[j][2] = natKeys[natIndex+4+j-(natKeys.length*2)];
+//			}
+//		}
+//		int accCount = -1;
+//		for(int a = 0; a<sharpNum.length; a++){
+//			if(key.equals(sharpNum[a])){
+//				accCount = a;
+//				for(int b = 0; b<accCount; b++){
+//					for(int c = 0; c<chords.length; c++){
+//						for(int d = 0; d<chords[c].length; d++){
+//							if(chords[c][d].equals(sharpOrder[b].substring(0, 1))){
+//								chords[c][d] = sharpOrder[b];
+//							}
+//						}
+//					}
+//				}
+//				break;
+//			}
+//		}
+//		if(accCount<0){
+//			for(int e=0; e<flatNum.length; e++){
+//				if(key.equals(flatNum[e])){
+//					accCount = e;
+//					for(int f = 0; f<accCount; f++){
+//						for(int g = 0; g<chords.length; g++){
+//							for(int h = 0; h<chords[g].length; h++){
+//								if(chords[g][h].equals(flatOrder[f].substring(0, 1))){
+//									chords[g][h] = flatOrder[f];
+//								}
+//							}
+//						}
+//					}
+//					break;
+//				}
+//			}
+//		}
+//		for(int j=0; j<chords.length; j++){
+//			for(int k=0; k<chords[j].length;k++){
+//				System.out.print(chords[j][k]);
+//			}
+//			System.out.print("\n");
+//		}
+//		System.out.println("Chord Tones: ");
+//		for(int k=0; k<chords.length; k++){
+//			chordTones[k] = chords[k][0];
+//			System.out.println(chordTones[k]);
+//		}
+//	}
 	
 	public void pickKey(){
 		Random randy = new Random();
-		keyIndex = randy.nextInt(12);
+		keyIndex = randy.nextInt(keys.length);
 		key = keys[keyIndex];
 	}
 	
--- src/autowub/Verse.java
+++ src/autowub/Verse.java
@@ -1,12 +1,20 @@
 package autowub;
 
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Verse {
 	int numMeasures;
 	double numBeats;
 	int keyIndex;
 	double noteTypes[] = new double[]{.25,.25,.25,.25,.5,.5,1,1,.125,.125,.125,.125,.0625,.0625,.0625};
+	ArrayList<Double> trackRhythm = new ArrayList<Double>();
+	ArrayList<Boolean> sopFlow = new ArrayList<Boolean>();
+	boolean trueOrFalse[] = new boolean[]{true,false};
+	Note sopranoLine[];
+	Note bassLine[];
+	Note altoLine[];
+	Note tenorLine[];
 	
 	public void play(){
 		
@@ -14,18 +22,44 @@ public class Verse {
 	
 	public void create(String key, int pKeyIndex){
 		keyIndex = pKeyIndex;
-		createSoprano();
+		createRhythm();
 	}
 	
-	public void createSoprano(){
-		for(int i = numMeasures; i>0; i--){
+	public void createRhythm(){
+		boolean upFlow = true;
+		Random randy = new Random();
+		for(int i = 0; i<numMeasures; i++){
+			System.out.println("Measure: " + i);
+			upFlow = trueOrFalse[randy.nextInt(2)];
+			System.out.println(upFlow);
 			double beatsLeft = numBeats;
 			while(beatsLeft>0){
 				double noteDur = pickNoteDur(beatsLeft);
+				trackRhythm.add(noteDur);
+				sopFlow.add(upFlow);
 				System.out.println(noteDur);
-				beatsLeft -= noteDur;
+				beatsLeft = beatsLeft - noteDur;
 			}
 		}
+	}
+	
+	public void createSoprano(){
+		sopranoLine = new Note[trackRhythm.size()];
+		sopranoLine[0] = new Note(trackRhythm.get(0),pickNoteFromChord(Song.chords[0]),false,90);
+		for(int i = 1; i<trackRhythm.size(); i++){
+			
+		}
+	}
+	
+	public String pickNoteFromChord(String[] chord){
+		Random randy = new Random();
+		String note = chord[randy.nextInt(chord.length)];
+		return note;
+	}
+	
+	public String pickNotePitch(String startingPitch, boolean upFlow){
+		String pitch = startingPitch;
+		return pitch;
 	}
 	
 	public double pickNoteDur(double beatsLeft){
@@ -33,7 +67,7 @@ public class Verse {
 		Random randy = new Random();
 		noteType = noteTypes[randy.nextInt(noteTypes.length)];
 		while(true){
-			if(noteType<beatsLeft){
+			if(noteType<=beatsLeft){
 				break;
 			}else{
 				noteType = noteTypes[randy.nextInt(noteTypes.length)];
